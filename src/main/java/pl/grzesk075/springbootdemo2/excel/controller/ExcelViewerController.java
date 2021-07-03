18c1d98cafad851c406b86b936b12a8fba471973
Grzegorz Kuprianowicz <grzesk075@gmail.com>
2021-07-03 19:33:42 +0200

    Demo2 - thymeleaf form.

--- src/main/java/pl/grzesk075/springbootdemo2/excel/controller/ExcelViewerController.java
+++ src/main/java/pl/grzesk075/springbootdemo2/excel/controller/ExcelViewerController.java
@@ -4,7 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.grzesk075.springbootdemo2.excel.model.ExcelViewer;
+import pl.grzesk075.springbootdemo2.excel.model.FormData;
+
+import java.util.Locale;
 
 @Controller
 public class ExcelViewerController {
@@ -13,8 +18,17 @@ public class ExcelViewerController {
     private ExcelViewer excelViewerModel;
 
     @GetMapping("/viewer")
-    public String excelViewer(Model model) {
+    public String excelDisplay(Model model) {
         model.addAttribute("title", excelViewerModel.getSheet().getTitle());
+        model.addAttribute("formdata", new FormData());
+        return "/viewer";
+    }
+
+    @PostMapping("/viewer")
+    public String excelSubmit(@ModelAttribute FormData formData, Model model) {
+        model.addAttribute("title", excelViewerModel.getSheet().getTitle());
+        formData.setNewTitle(formData.getNewTitle().toUpperCase());
+        model.addAttribute("formdata", formData);
         return "/viewer";
     }
 }
---
+++ src/main/java/pl/grzesk075/springbootdemo2/excel/model/FormData.java
@@ -0,0 +1,13 @@
+package pl.grzesk075.springbootdemo2.excel.model;
+
+public class FormData {
+    private String newTitle;
+
+    public String getNewTitle() {
+        return newTitle;
+    }
+
+    public void setNewTitle(String newTitle) {
+        this.newTitle = newTitle;
+    }
+}
